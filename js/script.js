6909e757e8a6ae7c54fe4554e393a4750c6ddf4f
Jayashakthi28 <55621768+Jayashakthi28@users.noreply.github.com>
2020-12-09 08:24:36 +0530

    Added fully responsive Blog Scroller

--- js/script.js
+++ js/script.js
@@ -5,11 +5,30 @@ $(document).ready(function (){
         $nav.toggleClass('collapse');
     })
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        //loop:true,
         autoplay:true,
         autoplayTimeout:3000,
         dots:false,
         nav:true,
+        navText:[$('.owl-nav .owl-prev'),$('.owl-nav .owl-next')],
+        responsive:{
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
+            },
+            787:{
+                items:2,
+                nav:true,
+                loop:true
+            },
+            0:{
+                items:1,
+                nav:false,
+                loop:true
+            }
+
+        }
     }); 
    
    });
